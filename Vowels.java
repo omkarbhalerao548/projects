4794cf3a7e493d0cf6b9273a0dfccc97ab2df41e
omkarbhalerao548 <omkarbhalerao548@gmail.com>
2024-07-17 18:35:36 +0530

    Add files via upload

---
+++ Vowels.java
@@ -0,0 +1,25 @@
+package jbk.practical;
+
+public class Vowels {
+
+	public static void main(String[] args) {
+	
+		int number=1;    
+	    switch(number){ 
+	    case 0:   
+	    case 1:  
+	    case 2: 
+	    case 3:
+	    case 4:
+	    case 5:
+	    case 6:
+	    case 7:
+	    case 8:
+	    case 9:
+	    	System.out.println("It is a Digit");
+	    	break;
+	    default: 
+	    	System.out.println("It is not a Digit");
+	    }  
+	}  
+	}  
